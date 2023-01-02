6a2d7009cb11bcbe890b1d80c70233adcb225b7b
sahitmandapati <sahit.m@technovert.com>
2023-01-02 18:23:56 +0530

    initial setup

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React  from 'react';
+import './App.css';
+import LandingPage from './components/landingPage/LandingPage';
+
+const App = () => {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
+
