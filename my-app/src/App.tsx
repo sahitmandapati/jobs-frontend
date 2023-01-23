fe37f91b038deda871c358020aa3df048fd4272e
sahitmandapati <sahit.m@technovert.com>
2023-01-23 20:42:14 +0530

    refactor - clean up unnecessary files

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,35 +4,20 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  Navigate
+  // Navigate
 } from "react-router-dom";
 import LandingPage from './components/landingPage/LandingPage';
-import SignIn from './components/auth/SignIn';
-import SignUp from './components/auth/SignUp';
-import Home from './components/dashboard/Home';
-import useToken from './customHooks/useToken';
-import useAccountType from './customHooks/useAccountType';
 
 
 
 const App = () => {
 
-  const { token, setToken } = useToken();
-  const { accountType, setAccountType } = useAccountType();
-
-  // useEffect(() => {
-  //   console.log(token)
-  // })
-
-
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={token ? <Navigate to="/home" /> : <LandingPage />} />
-          <Route path="/signin" element={token ? <Navigate to="/home" /> : <SignIn setToken={setToken} setAccountType={setAccountType} />} />
-          <Route path="/signup" element={token ? <Navigate to="/home" /> : <SignUp />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<LandingPage />} />
+          
         </Routes>
       </BrowserRouter>
     </div>
